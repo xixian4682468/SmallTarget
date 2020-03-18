769267287b0b3873a9ead96964d1397104c6e427
xixian4682468 <571079292@qq.com>
2020-03-18 22:07:32 +0800

    删除main.h,增加config.h文件

---
+++ config.h
@@ -0,0 +1,33 @@
+/********************************************************************************
+
+ **** Copyright (C), 2020, God Bless You info&tech Co., Ltd.                 ****
+
+ ********************************************************************************
+ * File Name     : main.h
+ * Author        : Ï£ÏÍ
+ * Date          : 2020-03-18
+ * Description   : .C file function description
+ * Version       : 1.0
+ * Function List :
+ * 
+ * Record        :
+ * 1.Date        : 2020-03-18
+ *   Author      : Ï£ÏÍ
+ *   Modification: Created file
+
+********************************************************************************/
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define		IMAGEWIDTH				640			//Í¼Ïñ¿í¶È
+#define		IMAGEHIGTH				512			//Í¼Ïñ¿í¶È
+
+
+
+void SelectInitialTrackRegion();
+void ProcWinAdapt();
+void SUANFAFUWEI();
+void FPGA_Config();
+
+
+#endif /* CONFIG_H_ */
